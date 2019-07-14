57ea500514b09e4c80cc43474666a92655b03bb4
MalouBron <m.bron.3@student.rug.nl>
2019-07-14 12:44:23 +0200

    finally got the SwiftyButton  cocoa pods working. However, SwiftyButton doesn't have a isHidden function so I need to come up with a solution for that

--- HAVO Engels/Controller/ViewController.swift
+++ HAVO Engels/Controller/ViewController.swift
@@ -5,17 +5,25 @@
 //  Created by Malou Bron on 08/07/2019.
 //  Copyright © 2019 Malou Bron. All rights reserved.
 //
+// make buttons nice
 
 import UIKit
-import FontAwesomeKit
+//import FontAwesomeKit
+//import ButtonAppearance
+import SwiftyButton
 
 class ViewController: UIViewController {
+    
     
     //global var score
     var score = 0
     var totalAnsweredQuestions = 0
     
+    
     //UI elements linked to IB actions
+    
+    @IBOutlet weak var submitOutlet: PressableButton!
+    @IBOutlet weak var nextWordOutlet: PressableButton!
     @IBOutlet weak var questionlabel: UILabel!
     @IBOutlet weak var scorelabel: UILabel!
     @IBOutlet weak var progresslabel: UILabel!
@@ -23,21 +31,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultlabel: UILabel! //shows if answer is correct or false
     @IBOutlet weak var rightword: UILabel! //if answer is wrong, shows the right answer
     
-    @IBOutlet weak var submitButton: UIButton!
-    
-    @IBOutlet weak var nextWordLabel: UIButton!
-    
-    @IBAction func nextword(_ sender: Any) {
-        let newWord = randomQuestion((Any).self)
-        questionlabel.text = newWord //sets the label automatically to a new word
-        rightword.text = "" //right word label disappears
-        submitButton.isHidden = false
-        nextWordLabel.isHidden = true
-    }
-    
     // This gets called when the UIViewController is loaded into memory when the app starts
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nextWordOutlet.colors = .init(button: .cyan, shadow: .blue)
+        nextWordOutlet.disabledColors = .init(button: .gray, shadow: .darkGray)
+        nextWordOutlet.shadowHeight = 10
+        nextWordOutlet.cornerRadius = 8
+        nextWordOutlet.depth = 0.5
+        
+        submitOutlet.colors = .init(button: .cyan, shadow: .blue)
+        submitOutlet.disabledColors = .init(button: .gray, shadow: .darkGray)
+        submitOutlet.shadowHeight = 10
+        submitOutlet.cornerRadius = 8
+        submitOutlet.depth = 0.5
         
         let firstQuestion = randomQuestion((Any).self)
         questionlabel.text = firstQuestion //displays now the question in the questionlabel
@@ -66,7 +74,16 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func submitlabel(_ sender: Any) {
+    @IBAction func nextWordButton(_ sender: Any) {
+        let newWord = randomQuestion((Any).self)
+        questionlabel.text = newWord //sets the label automatically to a new word
+        rightword.text = "" //right word label disappears
+        //submitButton(Any).isHidden = false
+        //nextWordButton(Any).isHidden = true
+    }
+
+    @IBAction func submitButton(_ sender: Any) {
+        
         let questionInLabel = questionlabel.text
         let answer = dict[questionInLabel!] //check if the ! is in correct use here. otherwise error : Cannot subscript a value of type '[String : String]' with an index of type 'String?'
         //let fittedAnswer = find(value: questionInLabel, in: allQuestions)
@@ -80,8 +97,8 @@ class ViewController: UIViewController {
             updateUI()
             
         } else {
-            nextWordLabel.isHidden = false
-            submitButton.isHidden = true //makes the submitButton disappear
+            //nextWordButton.isHidden = false
+            //submitButton.isHidden = true //makes the submitButton disappear
             rightword.text = "The correct answer is : \(String(describing: answer!))"
             resultlabel.text = "wrong"
             resultlabel.textColor = UIColor.red
