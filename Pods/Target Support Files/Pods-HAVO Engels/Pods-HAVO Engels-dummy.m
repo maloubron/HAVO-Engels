424717dc4cfbd7b120f1cafb10f38bfab99ca9e3
MalouBron <m.bron.3@student.rug.nl>
2019-07-16 12:33:20 +0200

    also the podifles are committed now and the workspace

---
+++ Pods/Target Support Files/Pods-HAVO Engels/Pods-HAVO Engels-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HAVO_Engels : NSObject
+@end
+@implementation PodsDummy_Pods_HAVO_Engels
+@end
---
+++ Pods/Target Support Files/Pods-HAVO EngelsTests/Pods-HAVO EngelsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HAVO_EngelsTests : NSObject
+@end
+@implementation PodsDummy_Pods_HAVO_EngelsTests
+@end
